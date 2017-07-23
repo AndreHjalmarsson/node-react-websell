6a7c28eaae6647352a6cac9cbc947ac017446658
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-07-23 13:37:42 +0200

    added error comment

--- server/Controllers/productController.js
+++ server/Controllers/productController.js
@@ -3,7 +3,9 @@ const User = require('../Models/UserModel');
 
 
 exports.addProduct = async (req, res) => {
+  // req.body.author = req.user._id;
   // const product = new Product(req.body);
   // await product.save();
+  // NO ACCESS TO REQ.USER...
   res.send(req.user);
 }
