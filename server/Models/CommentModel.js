940e7b949a3bec072adfc61cc8937cd1e00ffa56
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-09-18 18:48:17 +0200

    modified commentmodel

--- server/Models/CommentModel.js
+++ server/Models/CommentModel.js
@@ -1,15 +1,16 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const commentSchema = new mongoose.Schema({
-//   content: String,
-//   created: { type: Date, default: Date.now },
-//   author: {
-//     type: ObjectId,
-//     ref: 'User',
-//     required: 'Must have an author'
-//   }
-// });
+const commentSchema = new mongoose.Schema({
+  content: String,
+  productId: Number,
+  created: { type: Date, default: Date.now },
+  author: {
+    type: ObjectId,
+    ref: 'User',
+    required: 'Must have an author'
+  }
+});
 
-// const CommentModel = mongoose.model('Comment', commentSchema);
+const CommentModel = mongoose.model('Comment', commentSchema);
 
-// module.exports = CommentModel;
+module.exports = CommentModel;
