da6007ba59ffbfc1f9270341a8d6682b893419af
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-09-21 16:41:51 +0200

    more on fetching comments

--- server/Controllers/commentController.js
+++ server/Controllers/commentController.js
@@ -2,8 +2,16 @@ const Comment = require('../Models/CommentModel');
 
 exports.addComment = async (req, res) => {
   req.body.author = req.user;
-  console.log(req.body);
   const comment = new Comment(req.body);
   await comment.save();
   res.send({ message: 'Comment added' });
+};
+
+exports.getComments = async (req, res) => {
+  console.log(req.params);
+  const comments = await Comment.find({
+    productId: req.params.id
+  });
+  console.log(comments);
+  res.send(comments);
 };
